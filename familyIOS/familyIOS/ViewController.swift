cde6273dd6fe44bb0e8c1a251dbbd1548cd7311e
CC1993 <153718331@qq.com>
2017-02-17 15:14:54 +0800

    init ios code

---
+++ familyIOS/familyIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  familyIOS
+//
+//  Created by 侯哲 on 2017/2/17.
+//  Copyright © 2017年 北大软微挑战杯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
